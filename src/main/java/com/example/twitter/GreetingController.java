42800c1a0108fe3f582c3fb4a156a9531f23a106
Ostap Ptah <ostap.ptah@yandex.ua>
2019-12-17 10:41:42 +0200

    Filtering

--- src/main/java/com/example/twitter/GreetingController.java
+++ src/main/java/com/example/twitter/GreetingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -43,5 +44,19 @@ public class GreetingController {
 
     }
 
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+             messages = messageRepo.findByTag(filter);
+        }else {
+             messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+
+        return "main";
+    }
 
 }
